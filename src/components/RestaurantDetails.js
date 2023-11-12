25bc4ff73c6dc873ede821ff531fbffffe2a08a3
Mohit Khedekar <mohitkhedekar@gmail.com>
2023-11-12 15:48:08 +0530

    restaurant details updated

--- src/components/RestaurantDetails.js
+++ src/components/RestaurantDetails.js
@@ -1,16 +1,34 @@
 
 import { useParams } from "react-router-dom"
+import useRestaurantMenu from "../utils/useRestaurantMenu"
+import Shimmer from "./Shimmer";
+import { useState } from "react";
 
 
 const RestaurantDetails= ()=>{
+     const {id}= useParams();
 
+    // const [restMenuItems, setRestMenuItem]= useState([]);
 
-    const params= useParams();
-    console.log(params)
+     const menu_data= useRestaurantMenu(id)
+
+     if (menu_data === null) return <Shimmer />;
+
+     const itemCards =
+       menu_data?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]
+         ?.card?.card?.itemCards;
+
+    
     return (
         <>
             {/* <div>Restaurant id: {params}</div> */}
-            <div>Restaurant id: {params.id}</div>
+            <div>Restaurant id: {id}</div>
+
+            {itemCards.length===0 && <Shimmer/> }
+
+            {itemCards.map(item=>{
+                return <h1>{item.card.info.name}</h1>
+            })}
         </>
     )
 }
